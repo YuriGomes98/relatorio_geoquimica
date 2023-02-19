a108ef5a065f8c4108e5d3d77d81e1c8fad6c993
Yuri Gomes <ysgomes98@gmail.com>
2023-02-19 15:39:08 -0300

    20230219 last commit

--- script_test.R
+++ script_test.R
@@ -9,5 +9,11 @@ cmmi_dataset <-
     here::here("projeto_report_geoquimica/CriticalMineralDepositsGeochemistry.csv")) |> 
   janitor::clean_names()
 
-cmmi_south_america <- cmmi_dataset |>
-  dplyr::filter(cmmi_dataset$country %in% c("BRA", "ARG", "CHL", "BOL", "PER", "URY", "ECU", "VEN", "COL"))
+cmmi_americas <- cmmi_dataset |>
+  dplyr::filter(country %in% c("MEX", "USA", "CAN", "BRA", "ARG", "CHL", "BOL", "PER", "URY", "ECU", "VEN", "COL")) |>
+  dplyr::select(sample_name, primary_commodities, deposit_name, deposit_environment, country, 
+                state, cu_ppm, cu_detection_limit, cu_method, 
+                deposit_longitude_wgs84,deposit_latitude_wgs84)|>
+  drop_na(cu_ppm)|>
+  dplyr::filter(cu_ppm >= 50) |>
+  mutate(country = countrycode(country, "iso3c", "country.name")) 
